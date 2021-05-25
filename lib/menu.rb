76cb07a0e9d064a2ad0c7ce85cead97ae93594b4
Sawaez Amin <sawaezamin@gmail.com>
2021-05-25 15:24:56 -0600

    Make lib folder for files

---
+++ lib/menu.rb
@@ -0,0 +1,34 @@
+require "./game"
+
+class Menu
+
+  def get_input(message)
+  puts message
+  @input = gets.chomp
+    if @input == "i"
+      instructions
+
+    elsif @input == "p"
+      game = Game.new
+      game.begin
+
+    elsif @input == "q"
+      puts "Quitting..."
+    end
+
+    end
+
+
+    def instructions
+      puts "You are the 'Decoder'. Your job is to guess the combination of colors
+in under 10 tries. You will get a hint after every guess and the game continues until
+you run out of tries."
+    puts "Good Luck!"
+    puts "Press (b) to return to menu."
+
+
+  end
+end
+
+game = Menu.new
+game.get_input "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
